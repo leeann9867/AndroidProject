1750b22327bb0919778ddcfd9254ea846e3b4f71
DESKTOP-SHGJ33K\MinJong <dbalswhd113@naver.com>
2020-09-09 10:39:36 +0900

    20200909

--- app/src/main/java/com/example/folyfinalproject/JoinInfoActivity.kt
+++ app/src/main/java/com/example/folyfinalproject/JoinInfoActivity.kt
@@ -6,12 +6,11 @@ import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_join.*
 import kotlinx.android.synthetic.main.activity_joininfo.*
 
 class JoinInfoActivity : AppCompatActivity() {
 
-    private lateinit var auth : FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     private val db = FirebaseFirestore.getInstance()
 
@@ -23,15 +22,26 @@ class JoinInfoActivity : AppCompatActivity() {
 
 
         join_hu2.setOnClickListener {
-            if(join_info_email_area.text.toString() == " "){
-                Log.e("JoinInfoActivity", "실패")
-            }
-            else{
-                Log.e("JoinInfoActivity", "성공")
-                intent = Intent(this,LoginActivity::class.java)
-                startActivity(intent)
-            }
-        }
+            val nickname = hashMapOf(
 
+                "nickname" to join_info_google_email_area.text.toString()
+
+            )
+
+            db.collection("nickname")
+                .document(auth.currentUser?.uid.toString())
+                .set(nickname)
+                .addOnSuccessListener {
+                    Log.e("JoinInfoActivity", "성공")
+                    intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener {
+                    Log.e("JoinInfoActivity", "실패")
+
+                }
+
+        }
     }
 }
+
